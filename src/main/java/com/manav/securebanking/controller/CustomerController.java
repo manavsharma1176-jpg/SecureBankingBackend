59694256cb1464926f1a63860b2bc66990a69b16
Manav Sharma <manavsharma1176@gmail.com>
2026-09-07 19:55:47 +0530

    Day 21: Continue customer API development

--- src/main/java/com/manav/securebanking/controller/CustomerController.java
+++ src/main/java/com/manav/securebanking/controller/CustomerController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/customers")
 public class CustomerController {
 
+    //customer
     private CustomerService customerService;
 
     public CustomerController(CustomerService customerService){
